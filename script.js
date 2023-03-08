9ef6a87b89ef5e7e2c99b3743806d8e8d4f17feb
Pranav Bokade <89243706+bokadepranav@users.noreply.github.com>
2023-03-08 23:18:53 +0530

    form validation updated

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ if (close) {
 		nav.classList.remove('active');
 	})
 }
+
+function validateForm() {
+  let x = document.forms["myForm"]["name"].value;
+  let y = document.forms["myForm"]["email"].value;
+
+  var letters = /^[A-Za-z]+$/;
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+  if (x.match(letters)) {
+    alert("SignIn Successful");
+    // console.log("done....")
+  }
+  else {
+    alert("Name must be filled out properly");
+    return false;
+  }
+
+  if (y.match(mailformat)) {
+    console.log("done....")
+
+
+  }
+  else {
+    alert("You have entered an invalid email address!");
+
+  }
+}
+
